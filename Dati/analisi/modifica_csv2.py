709ab93bc735bb78ceb581bbc4e6a72bc1bfa20d
sghebs <tornesegabriel@gmail.com>
2025-01-10 14:59:39 +0100

    creato file per calcolare la media, manca da implementare con i file csv

--- Dati/analisi/modifica_csv2.py
+++ Dati/analisi/modifica_csv2.py
@@ -1,12 +1,28 @@
 import csv
 
 # Leggi il file CSV originale
-with open('medie_prova.csv', 'r') as infile:
+with open('prova.csv', 'r') as infile:
     reader = csv.reader(infile)
+    next(reader)
+    dati = [(int(tempo), float(temp)) for tempo, temp in reader]
+
     
     # Stampa le prime 5 righe
-    for i, row in enumerate(reader):
-        if i < 5:
-            print(row)
-        else:
-            break
+    i = 0
+    sum = 0
+    while i < len(dati):
+        for j in range(5):
+            if i < len(dati):
+            #print(dati[i])
+                sum += dati[i][1]
+                i += 1
+            else: 
+                sum = sum / (j)
+                break
+        if i < len(dati): sum = sum / 5
+
+        print(dati[i-1][0], sum)
+        sum = 0
+                
+        
+
